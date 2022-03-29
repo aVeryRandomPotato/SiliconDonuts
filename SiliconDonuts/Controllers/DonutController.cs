bebcbd58c64f18eda2451d9ca92224e0446d649c
aVeryRandomPotato <chase.stancik@gmail.com>
2022-03-28 19:48:25 -0600

    Added and Fixed DonutController Home Page

--- SiliconDonuts/Controllers/DonutController.cs
+++ SiliconDonuts/Controllers/DonutController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Logging;
+using SiliconDonuts.Models;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SiliconDonuts.Controllers
--- SiliconDonuts/Controllers/HomeController.cs
+++ SiliconDonuts/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SiliconDonuts.Models;
 
+
 namespace SiliconDonuts.Controllers
 {
     public class HomeController : Controller
